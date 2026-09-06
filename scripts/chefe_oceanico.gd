002bed77fae5192beb3125999ccaf6ba775a1f3f
Paulo Gomes <paulogomesextp@gmail.com>
2026-09-06 05:16:35 +0100

    Corrige compilacao do chefe oceanico

--- scripts/chefe_oceanico.gd
+++ scripts/chefe_oceanico.gd
@@ -2,7 +2,7 @@ class_name ChefeOceanico
 extends ChefeGenerico
 ## Bosses próprios do lote VII/VIII: formas e ataques de água/energia.
 @export_enum("estrela", "capitao", "leviata", "nereia", "devorador", "abismo") var forma := "estrela"
-var _t := 0.0
+var _tempo_ataque := 0.0
 var _cd := 0.0
 
 func _ready() -> void:
@@ -14,7 +14,7 @@ func _ready() -> void:
 	queue_redraw()
 
 func _process(dt: float) -> void:
-	_t += dt; _cd -= dt
+	_tempo_ataque += dt; _cd -= dt
 	if _cd <= 0.0: _cd = 0.7 if forma in ["abismo","leviata"] else 1.25; _onda()
 	queue_redraw()
 
@@ -23,13 +23,13 @@ func _draw() -> void:
 	if forma == "estrela":
 		var pts:=PackedVector2Array(); for i in 10: var a=-PI/2.0+TAU*i/10.0; var r=52.0 if i%2==0 else 22.0; pts.append(Vector2(cos(a),sin(a))*r); draw_colored_polygon(pts,c)
 	elif forma == "leviata":
-		draw_ellipse(Vector2.ZERO,Vector2(72,36),c); draw_circle(Vector2(-28,-4),10,Color.WHITE); draw_circle(Vector2(28,-4),10,Color.WHITE)
+		_desenhar_elipse(Vector2.ZERO,Vector2(72,36),c); draw_circle(Vector2(-28,-4),10,Color.WHITE); draw_circle(Vector2(28,-4),10,Color.WHITE)
 	elif forma == "abismo":
 		draw_circle(Vector2.ZERO,48,Color("140c2b")); for i in 6: draw_circle(Vector2(cos(i)*34,sin(i)*34),9,c)
 	else:
 		draw_circle(Vector2(0,-35),24,Color("17243b")); draw_colored_polygon(PackedVector2Array([Vector2(-34,-16),Vector2(34,-16),Vector2(25,55),Vector2(-25,55)]),Color("17243b")); draw_line(Vector2(18,-5),Vector2(65,-45),c,7)
 
-func draw_ellipse(centro:Vector2, raio:Vector2, cor:Color) -> void:
+func _desenhar_elipse(centro:Vector2, raio:Vector2, cor:Color) -> void:
 	var p:=PackedVector2Array(); for i in 24: var a=TAU*i/24.0; p.append(centro+Vector2(cos(a)*raio.x,sin(a)*raio.y)); draw_colored_polygon(p,cor)
 
 func _onda() -> void:
